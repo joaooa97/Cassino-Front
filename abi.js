f3ce27d8a7fb44d0ae930ae4315cc0cc13e08033
Diogo Souza <di0g0-dg@Hotmail.com>
2019-12-04 13:32:59 -0300

    Mudança contrato cassino e etc.

--- abi.js
+++ abi.js
@@ -2,302 +2,7 @@ var abiCassino = [
 	{
 		"constant": true,
 		"inputs": [],
-		"name": "premioAtualLoteria",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "tipoAposta",
-				"type": "uint8"
-			},
-			{
-				"name": "aposta",
-				"type": "uint8"
-			},
-			{
-				"name": "valor",
-				"type": "uint16"
-			}
-		],
-		"name": "apostarRoleta",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint8"
-			},
-			{
-				"name": "",
-				"type": "uint8"
-			},
-			{
-				"name": "",
-				"type": "bool"
-			},
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "verSaldoEther",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "id",
-				"type": "uint8"
-			},
-			{
-				"name": "novoEndereco",
-				"type": "address"
-			}
-		],
-		"name": "alterarContratoJogos",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "valorAposta",
-				"type": "uint256"
-			}
-		],
-		"name": "apostarCacaNiqueis",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint8[3]"
-			},
-			{
-				"name": "",
-				"type": "bool"
-			},
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [],
-		"name": "comprarFichas",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"payable": true,
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "opcao",
-				"type": "uint8"
-			},
-			{
-				"name": "novoMultiplicador",
-				"type": "uint8"
-			}
-		],
-		"name": "mudarMultiplicadoresRoleta",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "bilhete",
-				"type": "uint8[5]"
-			}
-		],
-		"name": "comprarBilheteLoteria",
-		"outputs": [
-			{
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [],
-		"name": "transferirTudoParaDono",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "verMeusBilhetesLoteria",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint8[][]"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "novoEndereco",
-				"type": "address"
-			}
-		],
-		"name": "alterarContratoToken",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "qtdFichas",
-				"type": "uint256"
-			}
-		],
-		"name": "resgatarEthers",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [],
-		"name": "sortearLoteria",
-		"outputs": [
-			{
-				"name": "",
-				"type": "address[]"
-			},
-			{
-				"name": "",
-				"type": "uint8[5]"
-			},
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "verMultiplicadoresCacaNiqueis",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint8[9]"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "verMultiplicadoresRoleta",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint8"
-			},
-			{
-				"name": "",
-				"type": "uint8"
-			},
-			{
-				"name": "",
-				"type": "uint8"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"name": "novoCompra",
-				"type": "uint256"
-			},
-			{
-				"name": "novoResgate",
-				"type": "uint256"
-			}
-		],
-		"name": "mudarValoresFCH",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "checarSaldoDaRoleta",
+		"name": "saldoMaquina",
 		"outputs": [
 			{
 				"name": "",
@@ -316,11 +21,39 @@ var abiCassino = [
 				"type": "uint8"
 			},
 			{
-				"name": "novoMultiplicador",
+				"name": "novoValor",
 				"type": "uint8"
 			}
 		],
-		"name": "mudarMultiplicadoresCacaNiqueis",
+		"name": "mudarMultiplicadores",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "sacarPremio",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "valorAposta",
+				"type": "uint256"
+			}
+		],
+		"name": "fazerAposta",
 		"outputs": [],
 		"payable": false,
 		"stateMutability": "nonpayable",
@@ -329,11 +62,11 @@ var abiCassino = [
 	{
 		"constant": true,
 		"inputs": [],
-		"name": "checarSaldoDoCacaNiqueis",
+		"name": "verMultiplicadores",
 		"outputs": [
 			{
 				"name": "",
-				"type": "uint256"
+				"type": "uint8[9]"
 			}
 		],
 		"payable": false,
@@ -341,10 +74,22 @@ var abiCassino = [
 		"type": "function"
 	},
 	{
+		"constant": false,
 		"inputs": [],
+		"name": "iniciarGiro",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint8[3]"
+			},
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
 		"payable": false,
 		"stateMutability": "nonpayable",
-		"type": "constructor"
+		"type": "function"
 	}
 ]
 
